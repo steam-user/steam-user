ec2c5fbc7593b4ceedc6d78cee0d36b1f7b5aa1a
James Grodzicki <jamespgrodzicki@gmail.com>
2019-09-24 09:56:17 -0500

    commiting stuff

--- weather-api.js
+++ weather-api.js
@@ -13,6 +13,10 @@ $("#location-input").keyup(function(e){
 $("#weather-search").on("click", function(e){
     e.preventDefault();
     searchLocation = $("#location-input").val().trim();
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    if(alphabet.includes(searchLocation)){
+        console.log("hi");
+    }
     queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
     "zip=" + searchLocation + "&units=imperial&appid=" + APIKey;
     console.log(queryURL);
@@ -26,6 +30,29 @@ $("#weather-search").on("click", function(e){
         $(".temp").text("Temperature (F) " + response.main.temp);
         $(".city").text("City: " + response.name);
         $(".weather").text(response.weather[0].description);
+        var weatherDesc = response.weather[0].description;
+        if(weatherDesc == "clear sky"){
+            $(".wrapping").attr("id", "clear");
+        }
+        else if(weatherDesc == "few clouds"){
+            $(".wrapping").attr("id", "few-clouds");
+        }
+        else if(weatherDesc == "scattered clouds"){
+            $(".wrapping").attr("id", "scattered-clouds");
+        }
+        else if(weatherDesc == "broken clouds"){
+            $(".wrapping").attr("id", "broken-clouds");
+        }
+        else if(weatherDesc == ""){
+            $(".wrapping").attr("id", "");
+        }
+        else if(weatherDesc == ""){
+            $(".wrapping").attr("id", "");
+        }
+        else if(weatherDesc == ""){
+            $(".wrapping").attr("id", "");
+        }
+
         console.log(response);
     });
 });
